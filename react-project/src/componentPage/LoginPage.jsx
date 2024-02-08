754f8766fe60ce91d888af02e9cda2e85453f8db
MambrosePersonal <mr.mikeambrose@gmail.com>
2024-02-08 13:12:34 -0500

    working nav bar

---
+++ react-project/src/componentPage/LoginPage.jsx
@@ -0,0 +1,20 @@
+export function LoginPage(){
+    return(
+        <>
+        <h1>Login Page</h1>
+        <form>
+        <label>
+          Username:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <div/>
+        <label>
+          Password:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      </>
+    )
+}
