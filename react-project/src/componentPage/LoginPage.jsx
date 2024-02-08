a6d2471b8514a9738e084b31b40dc379480fac4e
Nicky Rathe <nicky@rathe.com>
2024-02-08 16:50:32 -0500

    Updated login page

--- react-project/src/componentPage/LoginPage.jsx
+++ react-project/src/componentPage/LoginPage.jsx
@@ -1,20 +1,53 @@
+import React, { useState } from 'react';
+
 export function LoginPage(){
+  // Define state variables to hold form data
+  const [formData, setFormData] = useState({
+    name: '',
+    password: ''
+  });
+
+  // Event handler to update form data state on input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Event handler to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.name = 'nicky' && formData.password == 'password') {
+      console.log("Yay!")
+      //Add in the route to manager page
+    } else {
+      console.log("Bummer")
+    }
+
+    console.log(formData);
+    // Reset form fields
+    setFormData({
+      name: '',
+      password: ''
+    });
+  };
+
+
     return(
         <>
         <h1>Login Page</h1>
-        <form>
-        <label>
-          Username:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <div/>
-        <label>
-          Password:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" value={formData.password} onChange={handleInputChange} />
+      </div>
+   
+      <button type="submit">Submit</button>
+    </form>
+
       </>
     )
 }
