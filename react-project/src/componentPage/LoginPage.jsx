e84433b9faff7ceebe747f1b06c4e37ed8ed3085
MambrosePersonal <mr.mikeambrose@gmail.com>
2024-02-09 11:25:57 -0500

    added login credentials

--- react-project/src/componentPage/LoginPage.jsx
+++ react-project/src/componentPage/LoginPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { NavBar } from '../components/NavBar';
+import loginCred from '../componentPage/loginCred.json'
 
 export function LoginPage(){
-
   const navigate = useNavigate();
 
   // Define state variables to hold form data
@@ -21,12 +21,16 @@ export function LoginPage(){
   // Event handler to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (formData.name = 'nicky' && formData.password == 'password') {
-      navigate('/projects', { replace: true } )
-      //Add in the route to manager page
-    } else {
-      alert('Form validation failed')
-    }
+    for (let i = 0; i < loginCred.length; i++) {
+      if (formData.name === loginCred[i].username && formData.password === loginCred[i].password) {
+        navigate('/projects', { replace: true } )
+        break
+      } else if(formData.name !== loginCred[loginCred.length-1].username && formData.password !== loginCred[loginCred.length-1].password)
+        alert('Form validation failed')
+      }
+      
+    
+    
 
     console.log(formData);
     // Reset form fields
