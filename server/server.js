d3351580e721523ba5d30f0609cf6b5139a8b2c5
Nicky Rathe <nicky@rathe.com>
2024-02-09 10:35:43 -0500

    updated server for tasks route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 
-
-
 var url = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(url);
 const dbName = "proj-mgmt";
@@ -11,12 +9,18 @@ const app = express();
 
 app.use(express.json());
 
-
-
 app.all("*", (req, res, next) => {
   console.log(req.url);
   next();
 });
+
+async function getCollectionIdAsObject(id) {
+  const db = client.db(dbName);
+  const collection = db.collection('projects');
+  const data = await collection.findOne({'proj_id': +id});
+  return data;
+
+}
 
 app.get("/api/projects", async (req, res) => {
   try {
@@ -30,11 +34,25 @@ app.get("/api/projects", async (req, res) => {
   }
 });
 
-// app.use("", express.static("public/dist/index.html"));
-// app.use("/index.html", express.static("public/dist/index.html"));
-// app.use("/assets", express.static("public/dist/assets"));
-// app.use(express.static("public"));
+app.get("/api/projects/:id", async (req, res) => {
+  try {
+    const data = await getCollectionIdAsObject(req.params.id);
+    if(data) {
+      res.json(data);
+    } else {
+      res.status(404).json({message: 'Project not found'});
+    }
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+// app.use("", express.static("react-project/dist/index.html"));
+// app.use("/index.html", express.static("react-project/dist/index.html"));
+// app.use("/assets", express.static("react-project/dist/assets"));
+// app.use(express.static("react-project/public"));
 
 const port = 3500;
-client.close();
 app.listen(port, () => console.log(`Listening on port ${port}.`));
+
+client.close();
